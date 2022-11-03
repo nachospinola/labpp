314806c5526e0b3f15bc51dff1d214f548bd9b94
Nachospinola <luizdrive13@gmail.com>
2022-11-03 10:01:02 -0300

    Básico1

---
+++ aritmInt.c
@@ -0,0 +1,19 @@
+/*
+Arquivo: aritmInt.c
+Autor: Luiz Felipe Spinola Silva
+Matrícula:
+Criado em: 03/11/2022
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int a, b;
+    printf("digite dois numeros: \n");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d= %d\n", a, b, a+b);
+    printf("%d - %d= %d\n", a, b, a-b);
+    printf("%d * %d= %d\n", a, b, a*b);
+    printf("%d / %d= %d\n", a, b, a/b);
+    printf("%d %% %d= %d\n", a, b, a%b);
+    return 0;
+}
