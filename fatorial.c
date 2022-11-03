ecf5a42e3ede1eaad6f23040f0765279f16afd15
Nachospinola <luizdrive13@gmail.com>
2022-11-03 10:41:34 -0300

    galo

---
+++ fatorial.c
@@ -0,0 +1,18 @@
+/*
+Arquivo: aritmInt.c
+Autor: Luiz Felipe Spinola Silva
+Matrícula:
+Criado em: 03/11/2022
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int a, c=1;
+    scanf("%d", &a);
+    while(a>1){
+        c=c*a;
+        a--;
+    }
+    printf("%d\n", c);
+    return 0;
+}
