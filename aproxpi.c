a8207d89cb38a5ea097181c7b40ed9e1f37706f0
El Nacho <luizdrive13@gmail.com>
2023-01-05 10:37:59 -0300

    tt

---
+++ aproxpi.c
@@ -0,0 +1,28 @@
+/*
+Arquivo: aproxpi.c
+Nome: Luiz Felipe Spinola Silva
+Matricula: 12121EBI001
+Criado em 05/01/2023
+*/
+#include <math.h>
+#include <stdio.h>
+double S(int n);
+int main(void){
+    double x;
+    int n;
+    printf("Digite um numero:\n");
+    scanf("%d", &n);
+    printf("******* %lf\n", 5.6);
+    x = S(n);
+    printf("******* %lf\n", x);
+    return 0;
+}
+double S(int n){
+    if(n==0){
+        return 4.0;
+    }
+    return (S(n-1)+(4*((pow(-1,n))/((2*n)+1))));
+    //return (S(n-1)+(4.0*(((n%2==0)? 1.0 : -1.0)/((2*n)+1))));
+    
+}
+// iperetta@ufu.br
