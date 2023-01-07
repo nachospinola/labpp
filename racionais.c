e121e98c5821d7816e18ca4176ab388a576b677e
El Nacho <luizdrive13@gmail.com>
2023-01-06 23:18:43 -0300

    galooo

--- racionais.c
+++ racionais.c
@@ -1,3 +1,10 @@
+/*
+Arquivo: racionais.c
+Nome: Luiz Felipe Spinola Silva
+Matrícula: 12121EBI001
+Criado em: 06/01/2023
+*/
+#include <stdio.h>
 typedef
     struct Racional{
         int numerador, denominador;
@@ -13,6 +20,7 @@ int main(void){
     Racional Sub;
     Racional Mult;
     Racional Div;
+    int c;
     printf("Digite os valores para o numero racional:\n");
     scanf("%d %d %d %d", &Q.numerador, &Q.denominador, &P.numerador, &P.denominador);
     if((Q.denominador)<0){
@@ -24,11 +32,13 @@ int main(void){
         P.denominador=P.denominador*(-1);
         P.numerador=P.numerador*(-1);
     }
-    Q.numerador=Q.numerador/mdc(mod(Q.numerador),Q.denominador);
-    Q.denominador=Q.denominador/mdc(mod(Q.numerador),Q.denominador);
+    c=Q.numerador; //segura o valor do denominador para simplificar
+    Q.numerador=Q.numerador/mdc(mod(Q.numerador),mod(Q.denominador));
+    Q.denominador=Q.denominador/mdc(mod(c),mod(Q.denominador));
     //simplifiquei Q
+    c=P.numerador;
     P.numerador=P.numerador/mdc(mod(P.numerador),P.denominador);
-    P.denominador=P.denominador/mdc(mod(P.numerador),P.denominador);
+    P.denominador=P.denominador/mdc(mod(c),P.denominador);
     //simplifiquei P
     Soma.numerador=((Q.numerador)*(P.denominador))+((P.numerador)*(Q.denominador));
     Soma.denominador=(Q.denominador)*(P.denominador);
@@ -42,18 +52,26 @@ int main(void){
     Div.numerador=(Q.numerador)*(P.denominador);
     Div.denominador=(Q.denominador)*(P.numerador);
     //Acima Dividi
+    c=Soma.numerador;
     Soma.numerador=Soma.numerador/mdc(mod(Soma.numerador),Soma.denominador);
-    Soma.denominador=Soma.denominador/mdc(mod(Soma.numerador),Soma.denominador);
+    Soma.denominador=Soma.denominador/mdc(mod(c),Soma.denominador);
     //simplifiquei a soma.
+    c=Sub.numerador;
     Sub.numerador=Sub.numerador/mdc(mod(Sub.numerador),Sub.denominador);
-    Sub.denominador=Sub.denominador/mdc(mod(Sub.numerador),Sub.denominador);
+    Sub.denominador=Sub.denominador/mdc(mod(c),Sub.denominador);
     //simplifiquei a subtração
+    c=Mult.numerador;
     Mult.numerador=Mult.numerador/mdc(mod(Mult.numerador),Mult.denominador);
-    Mult.denominador=Mult.denominador/mdc(mod(Mult.numerador),Mult.denominador);
+    Mult.denominador=Mult.denominador/mdc(mod(c),Mult.denominador);
     //simplifiquei a multiplicação
+    c=Div.numerador;
     Div.numerador=Div.numerador/mdc(mod(Div.numerador),mod(Div.denominador));
-    Div.denominador=Div.denominador/mdc(mod(Div.numerador),mod(Div.denominador));
+    Div.denominador=Div.denominador/mdc(mod(c),mod(Div.denominador));
     //simplifiquei a divisão
+    if(Div.denominador<0){
+        Div.denominador=Div.denominador*(-1);
+        Div.numerador=Div.numerador*(-1);
+    }
     printf("%d/%d %d/%d %d/%d %d/%d %d/%d %d/%d", 
     Q.numerador, Q.denominador, P.numerador, P.denominador,
     Soma.numerador, Soma.denominador, Sub.numerador, Sub.denominador,
